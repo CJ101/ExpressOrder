2252ef0abe79a34d32773d6f44b61f5e12b3bf43
Joshua Myerson <myersonjosh1@gmail.com>
2016-07-05 09:03:20 -0400

    Fixed an issue
    Fixed an issue where cookies were not being stored,

--- media/scripts/process-order.js
+++ media/scripts/process-order.js
@@ -1,17 +1,17 @@
 // When the order is confirmed, create the cookies
 $("#orderForm").submit(function(event) {
 	// Set Order Selections
-	document.cookie = "entrees=" + entrees + " ; path=/; domain=.expressorder.brpromedia.com";
-	document.cookie = "fruitsVegetables1=" + fruitsVegetablesArray[0] + "; path=/; domain=.expressorder.brpromedia.com";
+	document.cookie = "entrees=" + entrees + " ; path=/;";
+	document.cookie = "fruitsVegetables1=" + fruitsVegetablesArray[0] + "; path=/;";
 	if (fruitsVegetablesArray[1] != null) {
-		document.cookie = "fruitsVegetables2=" + fruitsVegetablesArray[1] + "; path=/; domain=.expressorder.brpromedia.com";
+		document.cookie = "fruitsVegetables2=" + fruitsVegetablesArray[1] + "; path=/;";
 	}
-	document.cookie = "beverages=" + beverages + "; path=/; domain=.expressorder.brpromedia.com";
+	document.cookie = "beverages=" + beverages + "; path=/;";
 
 	// Disable Orders
-	document.cookie = "orderDisabled=true; path=/; domain=.expressorder.brpromedia.com";
+	document.cookie = "orderDisabled=true; path=/;";
 	
 	// Go to Order Placed page
 	event.preventDefault();
-	window.location = "http://expressorder.brpromedia.com/order-placed/";
+	window.location = "http://10.254.17.169:8082/order-placed/";
 });
